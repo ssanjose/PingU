ef651513be12cf1df916870745f2fba837009bc5
ssanjose <kurtsanjose58@gmail.com>
2025-03-07 21:52:08 -0600

    add: ErrPartnerNotFound

--- backend/internal/store/storage.go
+++ backend/internal/store/storage.go
@@ -9,16 +9,17 @@ import (
 )
 
 var (
-	ErrNotFound = errors.New("resource not found")
+	ErrNotFound        = errors.New("resource not found")
+	ErrPartnerNotFound = errors.New("partner not found")
 )
 
 type Storage struct {
 	Users interface {
 		Create(context.Context, *User) error
 		GetByID(context.Context, int64) (*User, error)
-		// Update(context.Context, *User) error
+		Update(context.Context, *User) error
 		Delete(context.Context, int64) error
-		Ping(context.Context) error
+		Ping(context.Context, *User) error
 	}
 }
 
