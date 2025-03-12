2cb346715d77588c2372d4980951857a1b2f05a8
ssanjose <kurtsanjose58@gmail.com>
2025-03-12 05:42:00 -0500

    create: user_invitations as token table for verification

---
+++ backend/cmd/migrate/migrations/000002_add_invitations.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS user_invitations (
+  token VARCHAR(255) PRIMARY KEY,
+  user_id BIGINT UNIQUE NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
